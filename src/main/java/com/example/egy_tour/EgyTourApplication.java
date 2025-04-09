40c603c4b8994bfc9df368ebf6514413075ae4fe
Mohamed Elkhattam <medo20333882@yahoo.com>
2025-04-09 17:45:51 +0200

    Adding project structure

---
+++ src/main/java/com/example/egy_tour/EgyTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.egy_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgyTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgyTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/egy_tour/EgyTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egy_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgyTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
