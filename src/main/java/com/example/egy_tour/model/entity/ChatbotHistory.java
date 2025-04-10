582cc32b2e89a5d8fb9fccebf023318176c93de4
Mohamed Elkhattam <medo20333882@yahoo.com>
2025-04-10 15:57:13 +0200

    Added chatBot history entity

---
+++ src/main/java/com/example/egy_tour/model/entity/ChatbotHistory.java
@@ -0,0 +1,34 @@
+package com.example.egy_tour.model.entity;
+
+import com.example.egy_tour.model.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "chatbot_history")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatbotHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "msg_content", nullable = false, columnDefinition = "TEXT")
+    private String msgContent;
+
+    @Column(name = "flag", nullable = false)
+    private boolean flag;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "time", nullable = false)
+    private Date time;
+}
