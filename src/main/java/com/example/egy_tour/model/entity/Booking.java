b68c5751761aea6dbcb9f88becd49bade034966d
Ahmed Taha <ahmed.t.taha.25@gmail.com>
2025-04-10 16:11:51 +0200

    Added Booking and Program models

---
+++ src/main/java/com/example/egy_tour/model/entity/Booking.java
@@ -0,0 +1,45 @@
+package com.example.egy_tour.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start_time")
+    private Date startTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end_time")
+    private Date endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_guide_id", nullable = false)
+    private User tourGuide;
+
+    @ManyToOne
+    @JoinColumn(name = "program_id", nullable = false)
+    private Program program;
+
+    @ManyToOne
+    @JoinColumn(name = "tourism_spot_id", nullable = false)
+    private TourismSpot tourismSpot;
+}
---
+++ src/main/java/com/example/egy_tour/model/entity/Program.java
@@ -0,0 +1,35 @@
+package com.example.egy_tour.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "programs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Program {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private Integer id;
+
+        @Column(name = "name")
+        private String name;
+
+        @Column(name = "description")
+        private String description;
+
+        @ManyToOne
+        @JoinColumn(name = "user_id", nullable = false)
+        private User user;
+
+        @OneToMany(mappedBy = "program")
+        private ArrayList<Booking> bookings;
+}
