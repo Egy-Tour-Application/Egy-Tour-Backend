a4865bbe8056042abd9d86e5603a9f3a27c6f240
boudyK <abdalrahmank2000@gmail.com>
2025-04-10 17:19:47 +0200

    adding repos

---
+++ src/main/java/com/example/egy_tour/DAO/TourismSpotRepository.java
@@ -0,0 +1,9 @@
+package com.example.egy_tour.DAO;
+
+import com.example.egy_tour.model.entity.TourismSpot;
+import com.example.egy_tour.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourismSpotRepository extends JpaRepository<TourismSpot, Integer> {
+
+}
---
+++ src/main/java/com/example/egy_tour/DAO/TourismSpotReviewRepository.java
@@ -0,0 +1,7 @@
+package com.example.egy_tour.DAO;
+
+import com.example.egy_tour.model.entity.TourismSpotReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourismSpotReviewRepository extends JpaRepository<TourismSpotReview, Integer> {
+}
