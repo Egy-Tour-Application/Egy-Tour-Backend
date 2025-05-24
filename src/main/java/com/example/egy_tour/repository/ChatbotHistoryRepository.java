02ddaa91f3b9bc759b2d1df9ae823b96faed7ab4
boudyK <abdalrahmank2000@gmail.com>
2025-05-24 15:01:31 +0300

    small typo

--- src/main/java/com/example/egy_tour/repository/ChatbotHistoryRepository.java
+++ src/main/java/com/example/egy_tour/repository/ChatbotHistoryRepository.java
@@ -1,4 +1,4 @@
-package com.example.egy_tour.DAO;
+package com.example.egy_tour.repository;
 
 import com.example.egy_tour.model.ChatbotHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
