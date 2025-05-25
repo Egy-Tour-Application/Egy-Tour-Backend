fd3f4550da988f30f1d08760856fdfc7493f8c8a
Ahmed Taha <ahmed.t.taha.25@gmail.com>
2025-05-25 19:09:24 +0300

    Modified user

--- src/main/java/com/example/egy_tour/model/User.java
+++ src/main/java/com/example/egy_tour/model/User.java
@@ -19,13 +19,13 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password")
