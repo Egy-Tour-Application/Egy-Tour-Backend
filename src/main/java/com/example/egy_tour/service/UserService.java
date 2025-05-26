b95f1526a5e190384e782812ed04402ca0891227
boudyK <abdalrahmank2000@gmail.com>
2025-05-27 00:20:44 +0300

    finishing user module

--- src/main/java/com/example/egy_tour/service/UserService.java
+++ src/main/java/com/example/egy_tour/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.egy_tour.service;
 
+import com.example.egy_tour.dto.UpdateUserDTO;
 import com.example.egy_tour.model.User;
 import com.example.egy_tour.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +20,18 @@ public class UserService {
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+
+    public User updateUser(Long id, UpdateUserDTO updateUserDTO) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setFirstName(updateUserDTO.getFirstName());
+            user.setLastName(updateUserDTO.getLastName());
+            user.setPhone(updateUserDTO.getPhone());
+            return userRepository.save(user);
+        }
+        return null;
     }
 
 
