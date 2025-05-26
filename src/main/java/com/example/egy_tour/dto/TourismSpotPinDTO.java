554283fab1789227a9004a3bae621469c1b11889
Anas Ahmed <anas.a.elyamanyy@gmail.com>
2025-05-26 17:15:02 +0300

    tourism spot dto modification

--- src/main/java/com/example/egy_tour/dto/TourismSpotPinDTO.java
+++ src/main/java/com/example/egy_tour/dto/TourismSpotPinDTO.java
@@ -17,8 +17,8 @@ public class TourismSpotPinDTO {
     private double egyptianPrice;
     private double foreignerPrice;
     private String address;
-    private Date openingTime;
-    private Date closingTime;
+    private String openingTime;
+    private String closingTime;
     private double latitude;
     private double longitude;
     private String image;
